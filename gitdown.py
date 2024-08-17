81cda434936f7a8e0d601c92c512a809eaf74a58
Clutchnp <clutch1356@proton.me>
2024-08-17 20:21:01 +0530

    directory creation fixes fixed a bug where program would return '' no such file or direvtory if there was no parent directory and also fixed a bug where the if one of the objects in a directory was a directory other files would be ignored

--- gitdown.py
+++ gitdown.py
@@ -3,33 +3,37 @@ import os
 from urllib.request import urlretrieve
 # expects a general url from github 
 # for eg https://github.com/Clutchnp/myvim/lua/core
+def makefile(download_url,rel,fullpath):
+    urlretrieve(download_url,os.path.relpath(fullpath,os.path.dirname(rel)))
 
-def makefile(download_url,path):
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    urlretrieve(download_url,path)
+def createdir():
+    pass
 
 def  urlmaker(url):
     a=url.split('/')
     newurl = f'https://api.github.com/repos/{a[3]}/{a[4]}/contents/{'/'.join(a[7:])}'
-    return newurl
+    os.mkdir(a[-1])
+    return newurl,'/'.join(a[7:])
 def innerfunc(object):
     if object['type'] == 'dir':
         urlmaker(object['html_url'])
 
-def upperfunc(newurl):
+def upperfunc(newurl,rel):
     request = requests.get(newurl)
     jsonresponse = request.json()
     for x in jsonresponse:
         if x['type'] == 'dir':
-            return upperfunc(x['url'])
+            os.makedirs(os.path.relpath(x['path'],os.path.dirname(rel)), exist_ok=True)
+            print(os.path.relpath(x['path'],os.path.dirname(rel)))
+            upperfunc(x['url'],rel)
         else :
-          makefile(x["download_url"],x["path"])
+          makefile(x["download_url"],rel,x['path'])
 
 
 if __name__ == "__main__":
-    url = "https://github.com/Clutchnp/myvim/lua/core"
-    a = urlmaker(url)
-    upperfunc(a)
+    url = "https://github.com/Clutchnp/myvim/tree/main/lua"
+    a,rel= urlmaker(url)
+    upperfunc(a,rel)
     print('done')
     
 
