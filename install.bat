7298bd919a9b00e4a52ee7f990b286613e673b2c
Clutchnp <clutch1356@proton.me>
2024-08-23 08:45:57 +0530

    installation script for windows

---
+++ install.bat
@@ -0,0 +1,39 @@
+@echo off
+
+setlocal
+pip show pyinstaller >nul 2>&1
+if errorlevel 1 (
+    echo PyInstaller not found. Installing PyInstaller...
+    pip install pyinstaller
+)
+pip install -r .\requirements.txt 
+
+echo Creating executable from %~1...
+pyinstaller --onefile --distpath dist %~1
+
+set SCRIPT_NAME=%~n1
+if not exist "dist\%SCRIPT_NAME%.exe" (
+    echo Failed to create executable.
+    exit /b 1
+)
+
+
+set DEST_DIR="C:\Users\%username%\AppData\Local\Programs\Python\Python312\Scripts\"
+
+if not exist "%DEST_DIR%" (
+    echo The directory "%DEST_DIR%" does not exist.
+    exit /b 1
+)
+
+copy "dist\%SCRIPT_NAME%.exe" "%DEST_DIR%"
+
+if exist "%DEST_DIR%\%SCRIPT_NAME%.exe" (
+    echo Executable %SCRIPT_NAME%.exe successfully copied to %DEST_DIR%.
+) else (
+    echo Failed to copy executable to %DEST_DIR%.
+    exit /b 1
+)
+
+endlocal
+exit /b 0
+
